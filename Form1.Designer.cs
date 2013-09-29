4c9bca557be47ba3461d6f7199963411cf75ba9a
GRIFFnDOOR <Griff.howl@gmail.com>
2013-09-28 19:24:20 -0700

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Web_Service_Tut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DefineButton = new System.Windows.Forms.Button();
+            this.WordBox = new System.Windows.Forms.TextBox();
+            this.DefinitionBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DefineButton
+            // 
+            this.DefineButton.Location = new System.Drawing.Point(161, 12);
+            this.DefineButton.Name = "DefineButton";
+            this.DefineButton.Size = new System.Drawing.Size(791, 38);
+            this.DefineButton.TabIndex = 0;
+            this.DefineButton.Text = "Define";
+            this.DefineButton.UseVisualStyleBackColor = true;
+            this.DefineButton.Click += new System.EventHandler(this.DefineButton_Click);
+            // 
+            // WordBox
+            // 
+            this.WordBox.Location = new System.Drawing.Point(12, 22);
+            this.WordBox.Name = "WordBox";
+            this.WordBox.Size = new System.Drawing.Size(143, 38);
+            this.WordBox.TabIndex = 2;
+            // 
+            // DefinitionBox
+            // 
+            this.DefinitionBox.Location = new System.Drawing.Point(12, 73);
+            this.DefinitionBox.Name = "DefinitionBox";
+            this.DefinitionBox.Size = new System.Drawing.Size(940, 267);
+            this.DefinitionBox.TabIndex = 3;
+            this.DefinitionBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.button1.Location = new System.Drawing.Point(12, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(940, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Show Word Document";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 389);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DefinitionBox);
+            this.Controls.Add(this.WordBox);
+            this.Controls.Add(this.DefineButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DefineButton;
+        private System.Windows.Forms.TextBox WordBox;
+        private System.Windows.Forms.RichTextBox DefinitionBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+using MSWord = Microsoft.Office.Interop.Word;
+using System.Reflection;
+using Regex = System.Text.RegularExpressions.Regex;
+
+namespace Web_Service_Tut
+{
+    public partial class Form1 : Form
+    {
+        Dict.DictServiceSoapClient DictClient;
+        Dict.WordDefinition DictServiceDef;
+
+        public Form1() {
+            InitializeComponent();
+            DictClient = new Dict.DictServiceSoapClient();
+            DictServiceDef = new Dict.WordDefinition();
+        }
+
+        private void DefineButton_Click(object sender, EventArgs e) { 
+            if (WordBox.Text.Length <= 2) return; //TODO: thow and handle exception
+            
+            // retrieve content from web service
+            DictServiceDef = DictClient.DefineInDict("gcide", WordBox.Text.Trim());
+            string Def = DictServiceDef.Definitions[0].WordDefinition,
+                   Word = DictServiceDef.Word,
+                   Type = ParseType(Def),
+                   Example = ParseExample(Def);
+
+            // add new definition to list
+            DefinitionBox.Text += String.Format( "{0} {1}: {2} \n\t- {3}\n\n", Word, FormatType(Type), FormatDef(Def), FormatExample(Example) );
+        }
+
+        private string ParseExample(string Example) {
+            do { //traverses & trims the definition until an example pops up
+                int start = Example.IndexOf("\n\n"); 
+                if (start >0) Example = Example.Remove(0, start +2).TrimStart();
+            } while ("0123456789".Contains(Example[0]));
+
+            int end = Example.IndexOfAny(".!?".ToCharArray()); 
+            if (end >0) Example = Example.Remove(end +1);
+
+            //TODO: Find a way to generate an example if none given by the web service
+            if (Example.IndexOf('{') >= 0) {return "No Example Could Be Generated";}
+            else return Example;
+        }
+
+        private string ParseType(string Def) {
+            // The word type is always before the first period
+            Def = Def.Remove(Def.IndexOf('.'));
+
+            string[,] Types = new string[,] { 
+                {"interj.","Interjection"}, {"conj.","Conjunction"}, {"prep.","Preposition"}, 
+                {"pron.","Pronoun"}, {"adv.","Adverb"}, {"v.","Verb"}, {"n.","Noun"}, {"a.","Adjective"}
+            };
+
+            for (int i= 0; i< Types.GetLength(0); i++) {
+                if (Def.IndexOf(Types[i,0]) >= 0) return Types[i,1];
+            } return "Unknown";
+        }
+
+        private string FormatType(string Type) {
+            return '(' + Type + ')';
+        }
+        private string FormatExample(string Example) {
+            return '"' + DelWhitespace(Example.Trim()) + '"';
+        }
+        private string FormatDef(string Def) {
+            return DelWhitespace(Trim(Def));
+        }
+
+        private string DelWhitespace(string String) {
+            return new Regex(@"\W+").Replace(String," ").Replace("\r\n", " ").Replace("\n", " ").Replace("\r", " ");
+        }
+        private string Trim(string Def) {
+            do { //trims front 
+                int start = Def.IndexOf("1."); if (start < 0)
+                    start = Def.IndexOf('\n'); if (start >= 0)
+                    Def = Def.Remove(0, start + 2);
+            } while (IsInBracket(Def, 0));
+
+            //trims end
+            char[] ends = {'.', '-'};
+            int end = Def.IndexOfAny(ends); if (end > 0)
+            Def = Def.Remove(end + 1);
+
+            return Def;
+        }
+
+        private bool IsInBracket(string String, int index) {
+            for (int i= index; i< String.Length; i++) {
+                if (String[i] == '[') return false;
+                if (String[i] == ']') return true;
+            } 
+            return false;
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        private MSWord.Application WordApp;
+        private MSWord.Document WordDoc;
+
+        private void button1_Click(object sender, EventArgs e) {InitializeDoc();}
+
+        private void InitializeDoc() {
+            object missing = System.Reflection.Missing.Value;
+
+            WordApp = new MSWord.Application();
+            WordDoc = WordApp.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+
+            WordApp.Visible = true;
+        }
+
+        private void AddDef(string Word, string Type, string Def, string Example)
+        {
+            Range DocRange = WordDoc.Range(WordDoc.Content.End - 1, WordDoc.Content.End);
+            DocRange.set_Style("No Spacing");
+            DocRange.Font.Size = 13.5f;
+            DocRange.Bold = 0;
+            DocRange.Font.Italic = 0;
+
+            //definition spacing
+            DocRange.Text = System.Environment.NewLine + System.Environment.NewLine;
+
+            //Word
+            DocRange.SetRange(WordDoc.Content.End - 1, WordDoc.Content.End);
+            {
+                DocRange.Text = Word + ' ';
+                DocRange.Font.Bold = -1;
+                DocRange.Font.Italic = 0;
+            }
+
+            //Type
+            DocRange.SetRange(WordDoc.Content.End - 1, WordDoc.Content.End);
+            {
+                DocRange.Text = Type + ':';
+                DocRange.Bold = 0;
+                DocRange.Italic = 0;
+            }
+
+            //Definition
+            DocRange.SetRange(WordDoc.Content.End - 1, WordDoc.Content.End);
+            {
+                DocRange.Text = Def;
+                DocRange.Bold = 0;
+                DocRange.Italic = 0;
+            }
+
+            //Example
+            DocRange.SetRange(WordDoc.Content.End - 1, WordDoc.Content.End);
+            {
+                DocRange.Text = "\n\t- " + Example;
+                DocRange.Bold = 0;
+                DocRange.Italic = 0;
+                DocRange.Font.Size = 11.0f;
+            }
+        } // AddDefinition()
+    } // Form1
+} // Namespace
